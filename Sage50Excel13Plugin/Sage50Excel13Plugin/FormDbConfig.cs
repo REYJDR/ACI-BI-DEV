ce7c8c5f9ad7ae8d95821f899229a6c77cd6e73c
Reinaldo Daou <reinaldo.daou@gmail.com>
2017-08-08 14:44:56 -0500

    connexion tab

---
+++ Sage50Excel13Plugin/Sage50Excel13Plugin/FormDbConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sage50Excel13Plugin
+{    
+    public partial class FormDbConfig : Form
+    {
+
+
+        private DbParam conParams;
+
+
+        public FormDbConfig()
+        {
+            InitializeComponent();
+            InitValue();
+        }
+
+
+        private void InitValue()
+        {
+            conParams = new DbParam();
+            conParams.GetValueFromFile();
+
+            textHost.Text = conParams.Hostaname;
+            textDb.Text   = conParams.Dbname;
+            textUser.Text = conParams.User;
+            textPass.Text = conParams.Password;
+
+
+        }
+
+        private void BtnDbSave_Click(object sender, EventArgs e)
+        {
+
+            /*INI READ AND SAVE CONNECTION PARAMETERS*/
+            conParams.Hostaname = textHost.Text;
+            conParams.User = textUser.Text;
+            conParams.Password = textPass.Text;
+            conParams.Dbname = textDb.Text;
+            /*END READ AND SAVE CONNECTION PARAMETERS*/
+
+            /*Save params values */
+            conParams.SetValueOnFile();
+
+
+        }
+    }
+}
