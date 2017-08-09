aa15145d886f9bc5e9978ed13546933d7eb34983
REYJDR <REINALDO.DAOU@GMAIL.COM>
2017-08-08 23:38:06 -0500

    RELEASE

--- Sage50Excel13Plugin/Sage50Excel13Plugin/Properties/AssemblyInfo.cs
+++ Sage50Excel13Plugin/Sage50Excel13Plugin/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -6,19 +7,19 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sage50Excel13Plugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ACI BI")]
+[assembly: AssemblyDescription("Sage50Excel13Plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sage50Excel13Plugin")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("DAOUTEL")]
+[assembly: AssemblyProduct("ACI BI")]
+[assembly: AssemblyCopyright("Copyright ©  APCON  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fd5a0213-542e-49e1-884d-967cbcbee645")]
@@ -35,4 +36,5 @@ using System.Security;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
 
