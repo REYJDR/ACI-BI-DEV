b61ee04f3d2d8279f92a5a14c1df190e79b3aa2e
REYJDR <REINALDO.DAOU@GMAIL.COM>
2017-08-08 23:43:34 -0500

    update 1.5

--- Sage50Excel13Plugin/Sage50Excel13Plugin/Properties/AssemblyInfo.cs
+++ Sage50Excel13Plugin/Sage50Excel13Plugin/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Security;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fd5a0213-542e-49e1-884d-967cbcbee645")]
